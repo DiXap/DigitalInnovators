9e944dd22ef8d49e7b7f34bef0d8764952b74dc0
Diego Padilla <dpadlara@ciencias.unam.mx>
2023-05-27 21:54:24 -0600

    feat: agrega el modelo de la base de datos para las categorias del menu

--- RestaurantApp/models.py
+++ RestaurantApp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Categories(models.Model):
+    name = models.CharField(max_length=200, unique=True, primary_key=True)
+
+    def __str__(self) -> str:
+        return self.name
