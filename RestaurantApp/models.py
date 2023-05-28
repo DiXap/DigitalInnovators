5dab038dfddf88cc06c37d480272300d7d256a53
Diego Padilla <dpadlara@ciencias.unam.mx>
2023-05-28 15:25:36 -0600

    feat: agrega la tabla correspondiente en la base de datos para manejar el carrito

--- RestaurantApp/models.py
+++ RestaurantApp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Categories(models.Model):
@@ -16,3 +17,14 @@ class Menu(models.Model):
 
     def __str__(self) -> str:
         return f'{self.name}: {self.category}'
+
+class CartItem(models.Model):
+    time = models.DateTimeField(auto_now_add=True)
+
+    item  = models.ForeignKey(Menu, on_delete=models.CASCADE, related_name='item')
+    table = models.ForeignKey(User, on_delete=models.CASCADE, related_name='table')
+
+
+    def __str__(self) -> str:
+        return f'{self.table.username} - {self.item.name} @ {self.time}'
+        # return f'@ {self.time}'
